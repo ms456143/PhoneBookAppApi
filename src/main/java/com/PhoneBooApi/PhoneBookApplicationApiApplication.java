3995ceb2d745af30b177c5e63fc962770185e3f9
ms456143 <ms456143@gmail.com>
2022-08-19 16:38:20 -0700

    PHON-1:Create Spring Starter Project with below dependencies

---
+++ src/main/java/com/PhoneBooApi/PhoneBookApplicationApiApplication.java
@@ -0,0 +1,13 @@
+package com.PhoneBooApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookApplicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookApplicationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PhoneBooApi/PhoneBookApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PhoneBooApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
